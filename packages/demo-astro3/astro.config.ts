e208e6bd900fe654757b49e914b1094c03c0fc06
Half-Gray <halfgray7e@gmail.com>
2024-06-26 03:21:58 -0700

    Add base option to Astro demos to test base directory handling

--- packages/demo-astro3/astro.config.ts
+++ packages/demo-astro3/astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import classicJs from '@halfgray/vite-plugin-classic-js';
 
 export default defineConfig({
+	base: '/demo',
 	vite: {
 		plugins: [classicJs()],
 	},
