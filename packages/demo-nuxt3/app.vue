c01ba8f5bea3b2563243bf9d863a29af70b156f3
Half-Gray <halfgray7e@gmail.com>
2024-06-26 15:23:49 -0700

    Note that classic script is not loaded in Nuxt dev

--- packages/demo-nuxt3/app.vue
+++ packages/demo-nuxt3/app.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import classicScript from './classic.cljs';
+
+const isDev = import.meta.env.DEV;
 </script>
 
 <template>
+	<p v-if="isDev">
+		<strong>Note</strong>: The classic script is not loaded in dev because
+		Nuxt uses a different path for filesystem ("/@fs/") URLs that is not
+		compatible with the plugin. However, it will be loaded in a production
+		build.
+	</p>
 	<p id="main"></p>
 	<component is="script" :src="classicScript"></component>
 </template>
